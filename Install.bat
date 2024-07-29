a418b1c5874681f7de8f94f513bbd1946ed5f78d
Doug <doug.stalker@gmail.com>
2024-07-29 16:40:18 +1000

    Initial version. Currently working: Crops, Trees, Berries, Herbs, Misc

---
+++ Install.bat
@@ -0,0 +1,1 @@
+copy "D:\TR\MoreHarvests\bin\Debug\netstandard2.1\net.nep.bepinex.moreharvests.dll"  "C:\Games\Steam\steamapps\common\Travellers Rest\Windows\BepInEx\plugins\"
---
+++ MoreHarvests.cs
@@ -0,0 +1,186 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+
+namespace MoreHarvests
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private static Harmony _harmony;
+        internal static ManualLogSource Log;
+
+        private static ConfigEntry<bool> _debugLogging;
+        private static ConfigEntry<int> _extraBerryCount;
+        private static ConfigEntry<int> _extraMiscCount;
+        private static ConfigEntry<int> _extraCropCount;
+        private static ConfigEntry<int> _extraTreeCount;
+        private static ConfigEntry<int> _extraHerbCount;
+
+
+        public Plugin()
+        {
+            // bind to config settings
+            _debugLogging = Config.Bind("Debug", "Debug Logging", false, "Logs additional information to console");
+            _extraBerryCount = Config.Bind("General", "Extra Berry Count", 2, "Number of extra berries to generate (set to 0 to disable)");
+            _extraMiscCount = Config.Bind("General", "Extra Misc Count", 2, "Number of extra misc items (sticks, junk, mussels) to generate (set to 0 to disable)");
+            _extraCropCount = Config.Bind("General", "Extra Crop Count", 2, "Number of extra crops/tree items to generate (set to 0 to disable)");
+            _extraTreeCount = Config.Bind("General", "Extra Tree Count", 2, "Number of extra tree items to generate (set to 0 to disable)");
+            _extraHerbCount = Config.Bind("General", "Extra Herb Count", 2, "Number of extra herbs to generate (set to 0 to disable)");
+
+        }
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Log = base.Logger;
+            _harmony = Harmony.CreateAndPatchAll(typeof(Plugin));
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        private void OnDestroy()
+        {
+            _harmony.UnpatchSelf();
+        }
+        public static void DebugLog(string message)
+        {
+            // Log a message to console only if debug is enabled in console
+            if (_debugLogging.Value)
+            {
+                Log.LogInfo(String.Format("NepMoreHarvests: Debug: {0}", message));
+            }
+        }
+
+        //////////////////////////////////////////////////////////////////
+        //  BushHarvest (berry bushes)
+
+        [HarmonyPatch(typeof(BushHarvest), "MouseUp")]
+        [HarmonyPostfix]
+        static void BushHarvestMouseUpPostfix(BushHarvest __instance, ref bool __result)
+        {
+            DebugLog(String.Format("In BushHarvest MouseUp Postfix: return value: {0}", __result ? "TRUE" : "FALSE"));
+            if (__result && (_extraBerryCount.Value > 0))
+            {
+                DroppedItem.SpawnDroppedItem(__instance.gameObject.transform.position, __instance.harvestedItems, _extraBerryCount.Value, false, false, 0);
+            }
+            return;
+        }
+
+        //////////////////////////////////////////////////////////////////
+        //  MiscellaneousHarvest (sticks, junk, shellfish)
+
+        [HarmonyPatch(typeof(MiscellaneousHarvest), "MouseUp")]
+        [HarmonyPostfix]
+        static void MiscellaneousHarvesttMouseUpPostfix(MiscellaneousHarvest __instance, ref bool __result)
+        {
+            DebugLog(String.Format("In MiscellaneousHarvest MouseUp Postfix: return value: {0}", __result ? "TRUE" : "FALSE"));
+            if (__result && (_extraMiscCount.Value > 0))
+            {
+                DroppedItem.SpawnDroppedItem(__instance.gameObject.transform.position, __instance.harvestedItems.item, _extraMiscCount.Value, false, false, 0);
+ 
+            }
+            return;
+        }
+
+
+        //////////////////////////////////////////////////////////////////
+        //  MiscItemHarvest (herbs)
+
+        [HarmonyPatch(typeof(MiscItemHarvest), "MouseUp")]
+        [HarmonyPostfix]
+        static void MiscItemHarvestMouseUpPostfix(MiscItemHarvest __instance, ref bool __result)
+        {
+            DebugLog(String.Format("In MiscItemHarvest MouseUp Postfix: return value: {0}", __result ? "TRUE" : "FALSE"));
+            if (__result && (_extraHerbCount.Value > 0))
+            {
+                DroppedItem.SpawnDroppedItem(__instance.gameObject.transform.position, __instance.harvestedItems.item, _extraHerbCount.Value, false, false, 0);
+
+            }
+            return;
+        }
+
+
+
+        //////////////////////////////////////////////////////////////////
+        //  Harvestable (crops, trees)
+
+        [HarmonyPatch(typeof(Harvestable), "HarvestAction")]
+        [HarmonyPrefix]
+        static void HarvestableHarvestActionPrefix(Harvestable __instance)
+        {
+            DebugLog("In Harvestable HarvestAction Prefix");
+
+            // The problem is this method goes into this.HEMOKEKCEFD(LCJKCBNBMHN)to do the actual dropping of the crops
+            // HEMOKEKCEFD : drops the items from this.harvestedItems[]
+            //               drops items with a probability from this.harvestedItemsProb[]
+            //               calls this.OLMBADPCGGC to actually place the items
+
+
+            if (__instance.cropSetter != null)
+            {
+                bool isTree = __instance.cropSetter.IsTreeCrop();
+                bool isGrown = __instance.cropSetter.growable.grown;
+                bool isHarvestable = __instance.isHarvestable;
+
+                int extraItems = 0;
+
+
+                if (isGrown && isHarvestable) // we're harvesting a crop or tree
+                {
+                    if (isTree)
+                    {
+                        DebugLog("In Harvestable HarvestAction Prefix: I think it's a tree");
+                        extraItems = _extraTreeCount.Value;
+                    } 
+                    else
+                    {
+                        DebugLog("In Harvestable HarvestAction Prefix: I think it's a crop");
+                        extraItems = _extraCropCount.Value;
+                    }
+                    
+                }
+                else 
+                {
+                    DebugLog("In Harvestable HarvestAction Prefix: No idea what is being harvested!");
+                }
+                DebugLog(String.Format("In Harvestable HarvestAction Prefix: Extra item count {0}", extraItems));
+
+                for (int i = 0; i < __instance.harvestedItems.Length; i++)
+                {
+                    // Need to get rid of random junk here or will break every patch
+                    // item.PNPJANDHIBH(true) ==> item.id (more or less...) because getting id directly is protected.
+                    // Lets try reflection!
+
+                    int reflectedItemID = 0;
+                    reflectedItemID = Traverse.Create(__instance.harvestedItems[i].item).Field("id").GetValue<int>();
+                    if (reflectedItemID != 0)
+                    { 
+                        //This is the "more or less" part that is done by function BIJCAFKFIOG
+                        if (reflectedItemID == 1224)
+                        {
+                            reflectedItemID = 1226;
+                        }
+                        if (Utils.dictReplaceItems.ContainsKey(reflectedItemID))
+                        {
+                            reflectedItemID = Utils.dictReplaceItems[reflectedItemID];
+                        }
+
+                        DebugLog(String.Format("In Harvestable HarvestAction Prefix: itemID {0}", reflectedItemID));
+                        //Item item = ItemDatabaseAccessor.GetItem(Utils.BIJCAFKFIOG(__instance.harvestedItems[i].item.PNPJANDHIBH(true), false), false, true); <-- Original line with "fun" functions
+                        Item item = ItemDatabaseAccessor.GetItem(reflectedItemID);
+                        if (item != null)
+                        {
+                            DroppedItem.SpawnDroppedItem(__instance.gameObject.transform.position, item, extraItems, false, false, 0);
+                        }
+                    }
+
+                }
+
+
+            }
+        }
+
+    }
+}
